b964358c146a80a2743d98637ceb36a64a5cd643
Afsal Thaj <afsal.taj06@gmail.com>
2024-10-31 21:21:10 +1100

    Fix links

--- check-links.js
+++ check-links.js
@@ -1,17 +1,17 @@
 const glob = require("glob")
 const { exec } = require("child_process")
 
-glob("**/*.mdx", (err, files) => {
+glob("**/js-language-guide/defining-components.mdx", (err, files) => {
   if (err) {
     console.error("Error finding files:", err)
     process.exit(1)
   }
 
   files.forEach(file => {
-    exec(`markdown-link-check ${file} --quiet`, (err, stdout, stderr) => {
+    exec(`markdown-link-check ${file}`, (err, stdout, stderr) => {
       if (err) {
         console.error(`Error checking links in ${file}:`, stderr || stdout)
-        process.exit(1) // Fail the script if any file has broken links
+        process.exit(1)
       } else {
         console.log(`Checked links in ${file}:`, stdout)
       }
